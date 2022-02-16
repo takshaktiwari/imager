bc7eb5ea9131531d73fbba1620a90f1541d927b2
Takshak Tiwari <takshaktiwari@gmail.com>
2022-02-16 16:36:26 +0530

    routes added in config condition

--- routes/web.php
+++ routes/web.php
@@ -5,6 +5,11 @@ use Takshak\Imager\Http\Controllers\PicsumController;
 use Takshak\Imager\Http\Controllers\PlaceholderController;
 
 Route::prefix('imgr')->name('imgr.')->group(function(){
-	Route::get('placeholder', [PlaceholderController::class, 'index'])->name('placeholder');
-	Route::get('picsum', [PicsumController::class, 'index'])->name('picsum');
+	if(config('site.imager.placeholder.enable_url', true)){
+		Route::get('placeholder', [PlaceholderController::class, 'index'])->name('placeholder');
+	}
+
+	if(config('site.imager.picsum.enable_url', true)){
+		Route::get('picsum', [PicsumController::class, 'index'])->name('picsum');
+	}
 });
